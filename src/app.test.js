78bc2c1a12b65f03767887d5d62e7a43cc89b6c7
Alexey Martishin <asmartishin@gmail.com>
2023-04-06 19:32:14 +0200

    MOV-2 Use short version of Thursday (#2)
    Use short version of Thursday
    
    ---------
    
    Co-authored-by: Alex Martishin <monkey@Alexs-MacBook-Pro.local>

--- src/app.test.js
+++ src/app.test.js
@@ -4,7 +4,7 @@ const cases = [
     [new Date('3/09/2020'), 'Monday'],
     [new Date('3/10/2020'), 'Tuesday'],
     [new Date('3/11/2020'), 'Wednesday'],
-    [new Date('3/12/2020'), 'Thursday'],
+    [new Date('3/12/2020'), 'Thur'],
     [new Date('3/13/2020'), 'Friday'],
     [new Date('3/14/2020'), 'Saturday'],
     [new Date('3/15/2020'), 'Sunday']
