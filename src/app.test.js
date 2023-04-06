31378eaa57e70ef02ccc2b4d526212c96e6b6b37
Alexey Martishin <asmartishin@gmail.com>
2023-04-06 02:44:04 +0200

    Add tests

--- src/app.test.js
+++ src/app.test.js
@@ -1,7 +1,16 @@
 const { dayOfTheWeek } = require('./app');
 
+const cases = [
+    [new Date('3/09/2020'), 'Monday'],
+    [new Date('3/10/2020'), 'Tuesday'],
+    [new Date('3/11/2020'), 'Wednesday'],
+    [new Date('3/12/2020'), 'Thursday'],
+    [new Date('3/13/2020'), 'Friday'],
+    [new Date('3/14/2020'), 'Saturday'],
+    [new Date('3/15/2020'), 'Sunday']
+];
 
-test('getDay returns the long-format day of the week', () => {
-    const day = dayOfTheWeek( new Date('3/11/2020') );
-    expect( day ).toBe('Wednesday');
+test.each(cases)('getDay returns the long-format day of the week', (date, expectedResult) => {
+    const result = dayOfTheWeek(date);
+    expect(result).toBe(expectedResult);
 });
